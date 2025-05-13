349408ebe17836964f2d5437c2b39a8eab8a0b73
Rafael <ouvidorianetcom@gmail.com>
2025-05-13 07:16:23 -0400

    Alterando a saída de objetos do tipo String usando as propriedades

---
+++ Main.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val nome = "RafaelBruno"
+    val tamanhoDoNome = nome.length //Acessando a propriedado do objeto String para mostrar quantos caracteres tem armazenado dentro da String
+
+    val nomeMaiusculo = "Rafael Bruno".uppercase() //Função que deixa todos os caracteres maiusculos
+    val nomeMinusculo = "Rafael Bruno".lowercase() //Função que deixa todos os caracteres minusculos
+
+    println(tamanhoDoNome)
+    println(nomeMaiusculo)
+    println(nomeMinusculo)
+
+}
