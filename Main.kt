30b92ec0f480343f0dc038e70705e3ef35fbd522
Rafael <ouvidorianetcom@gmail.com>
2025-05-21 06:42:51 -0400

    Segundo exemplo de controle de Fluxo com diversos else if's

--- Main.kt
+++ Main.kt
@@ -3,10 +3,16 @@ fun main() {
     val produto = "iPhone"
 
     //Se, Senão -> Deve ser uma expressão booleana
-    // (< , <= , > , >= , !)
-    if (produto.length <= 3) {
-        println("Você não pode cadastrar produtos com nome pequeno")
+    // (< , <= , > , >= , ! , == , != )
+
+    val preco = 20_000
+    if (preco > 30_000) {
+        println("Você ganhou 30% de desconto")
+    } else if (preco > 20_000) {
+        println("Você ganhou 20% de desconto")
+    } else if (preco > 10_000) {
+        println("Você ganhou 10% de desconto")
     } else {
-        println("Produto Cadastrado com sucesso")
+        println("O valor não é suficiente para aplicar um desconto")
     }
 }
