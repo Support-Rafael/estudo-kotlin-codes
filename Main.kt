44b1c171f98f1990a08ae43606b2d2f17018cb70
Rafael <ouvidorianetcom@gmail.com>
2025-05-23 06:27:24 -0400

    Adicionando comentários

--- Main.kt
+++ Main.kt
@@ -1,22 +1,30 @@
 fun main() {
+
+    //Solicitando informações sobre o problema enfrentado
     println("Qual foi o problema identificado? 1-Lentidão, 2-Quedas, 3-Sem Conexão")
     val solicitacao = readLine()
 
+    //Adicionando descrição do atendimento (Colocar todas informações)
     println("Adicione a observação do atendimento")
     val observacao = readLine()
 
+    //Entrada do nome do cliente
     println("Nome do cliente")
     val nome = readLine()
 
+    //Entrada do número do cliente
     println("Número do cliente")
     val telefone = readLine()
 
+    //Adicionar informação do problema principal relatado
     println("Observação do atendimento")
     val observacaoAtendimento = readLine()
 
+    //Adicionar localização fixa
     println("Localização do cliente")
     val localizacao = readLine()
 
+    //Condição para identificar qual problema de acordo com a entrada da variável "solicitacao"
     val lentidao = if (solicitacao?.contains("1") == true) {
         "x"
     } else {
@@ -35,6 +43,7 @@ fun main() {
         ""
     }
 
+    //Usando (""") para printar o texto inteiro formatado
     println("""
         CLIENTE APRESENTA QUAL PROBLEMA?
         SOLICITAÇÃO(x) - LENTIDÃO ( $lentidao ) - QUEDAS ($quedas) - SEM CONEXÃO ($loss)
@@ -45,6 +54,7 @@ fun main() {
         LOCALIZAÇÃO: $localizacao
     """.trimIndent())
 
+    //Usando as informações da N2 para preencher a N3 e finalizar ou encaminhar equipe
     println("""
         -QUAL O PROBLEMA RELATADO PELO CLIENTE?
          $observacaoAtendimento
