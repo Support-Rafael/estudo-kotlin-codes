d8235ffa0836064abba88532d36ed7802069cc32
Rafael <ouvidorianetcom@gmail.com>
2025-05-27 10:54:31 -0400

    Usando funções para adicionar organização e melhorar a eficiência e manutenção do código

---
+++ Funcoes.kt
@@ -0,0 +1,11 @@
+fun main() {
+    //Valores Nulos
+
+    // Functions - Blocos de códigos Reutilizáveis | Organizar o Código
+
+    olaMundo() //Call ou Chamada de Função
+
+}
+fun olaMundo() {
+    println("Olá, Mundo")
+}
