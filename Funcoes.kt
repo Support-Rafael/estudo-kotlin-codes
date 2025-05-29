bf08950b0af27514f51ff215fdeec16d3263a17b
Rafael <ouvidorianetcom@gmail.com>
2025-05-29 09:17:12 -0400

    Mostrando chamadas de função, funções sem retorno com Unit como tipo e como utilizar o return

--- Funcoes.kt
+++ Funcoes.kt
@@ -1,11 +1,21 @@
 fun main() {
     //Valores Nulos
-
+    //Toda função sem tipo de retorno declarado é do tipo : Unit
     // Functions - Blocos de códigos Reutilizáveis | Organizar o Código
+
+
+    val resposta = somar() //Valor da variável recebendo retorno da função
 
     olaMundo() //Call ou Chamada de Função
 
+    println(resposta) //Printado o resultado da chamada da função somar
 }
+//Função para somar dois números e printar o resultado
+fun somar(): String {
+    val resultado = 2 + 3
+    return "Estou somando 2 com 3 e o resultado é: $resultado"
+}
+//Função para retornar um texto
 fun olaMundo() {
-    println("Olá, Mundo")
+    println("Olá, mundo")
 }
